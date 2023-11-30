4a09408775ca2e17a8a974774f4e9b6575f0aa08
Eric Du <ericxdu1@gmail.com>
2023-11-30 13:02:14 -0500

    Added week 12 code

---
+++ Week 12/Class Completed Code/WebSocket/.history/client/src/App_20230628114548.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import useWebSocket from 'react-use-websocket';
+import './App.css';
+const WS_URL = 'ws://127.0.0.1:8000';
+function App() {
+ useWebSocket(WS_URL, {
+ onOpen: () => {
+ console.log('WebSocket connection established.');
+ }
+ });
+ return (
+ <div>Hello WebSockets!</div>
+ );
+}
+/* When a user joins, I notify the
+server that a new user has joined to edit the document. */
+function LoginSection({ onLogin }) {
+  const [username, setUsername] = useState('');
+  useWebSocket(WS_URL, {
+    share: true,
+    filter: () => false
+  });
+  function logInUser() {
+    if(!username.trim()) {
+      return;
+    }
+    onLogin && onLogin(username); // Triggers sendJsonMessage in App
+  }
+  // ----
+  // ----
+
+/* When content changes, we send the
+current content of the editor to the server. */
+function handleHtmlChange(e) {
+  sendJsonMessage({
+    type: 'contentchange',
+    content: e.target.value
+  });
+}
+
+return (
+  <DefaultEditor value={html} onChange={handleHtmlChange} />
+);
+export default App;
---
+++ Week 12/Class Completed Code/WebSocket/.history/client/src/App_20230628115453.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import useWebSocket from 'react-use-websocket';
+import './App.css';
+
+const WS_URL = 'ws://127.0.0.1:8000';
+
+function App() {
+  useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
+    }
+  });
+
+  return <div>Hello WebSockets!</div>;
+}
+
+/* When a user joins, I notify the
+server that a new user has joined to edit the document. */
+function LoginSection({ onLogin }) {
+  const [username, setUsername] = useState('');
+
+  /* When content changes, we send the
+  current content of the editor to the server. */
+  function handleHtmlChange(e) {
+    // Placeholder code for sending HTML content
+    console.log('HTML Content:', e.target.value);
+  }
+
+  return (
+    <div>
+      <textarea value={username} onChange={handleHtmlChange} />
+    </div>
+  );
+}
+
+export default App;
